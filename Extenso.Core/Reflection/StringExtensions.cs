0afd0bd6352807abaf584e8fb6beace9842101aa
vnmatt <gordon_matt@hotmail.com>
2018-06-23 10:48:50 +0930

    Refactoring and added more XML comments

--- Extenso.Core/Reflection/StringExtensions.cs
+++ Extenso.Core/Reflection/StringExtensions.cs
@@ -3,54 +3,104 @@ using System.Reflection;
 
 namespace Extenso.Reflection
 {
+    /// <summary>
+    /// Provides a set of static methods for parsing strings
+    /// </summary>
     public static class StringExtensions
     {
+        /// <summary>
+        /// If the specified type has a Parse(string) method, it will be invoked on the given input and the result returned.
+        /// </summary>
+        /// <param name="input">The System.String to parse.</param>
+        /// <param name="type">The type to examine for a Parse(string) method.</param>
+        /// <returns>The result, if the given type has a Parse(string) method; otherwise null.</returns>
         public static object ParseOrDefault(this string input, Type type)
         {
-            MethodInfo parseMethod = type.GetTypeInfo().GetMethod("Parse", new Type[] { typeof(string) });
+            var parseMethod = type.GetTypeInfo().GetMethod("Parse", new Type[] { typeof(string) });
 
             if (parseMethod != null)
             {
                 return parseMethod.Invoke(null, new string[] { input });
             }
+
             return null;
         }
 
-        public static TOut ParseOrDefault<TOut>(this string input)
+        /// <summary>
+        /// If the specified type has a Parse(string) method, it will be invoked on the given input and the result returned.
+        /// </summary>
+        /// <typeparam name="T">The type to examine for a Parse(string) method.</typeparam>
+        /// <param name="input">The System.String to parse.</param>
+        /// <returns>The result, if the given type has a Parse(string) method; otherwise default(T).</returns>
+        public static T ParseOrDefault<T>(this string input)
         {
-            return input.ParseOrDefault(default(TOut));
+            return input.ParseOrDefault(default(T));
         }
 
-        public static TOut ParseOrDefault<TOut>(this string input, TOut defaultValue)
+        /// <summary>
+        /// If the specified type has a Parse(string) method, it will be invoked on the given input and the result returned.
+        /// </summary>
+        /// <typeparam name="T">The type to examine for a Parse(string) method.</typeparam>
+        /// <param name="input">The System.String to parse.</param>
+        /// <param name="defaultValue">A value to return in case the specified type has no Parse(string) method.</param>
+        /// <returns>The result, if the given type has a Parse(string) method; otherwise defaultValue.</returns>
+        public static T ParseOrDefault<T>(this string input, T defaultValue)
         {
-            Type type = typeof(TOut);
-            MethodInfo parseMethod = type.GetTypeInfo().GetMethod("Parse", new Type[] { typeof(string) });
+            var type = typeof(T);
+            var parseMethod = type.GetTypeInfo().GetMethod("Parse", new Type[] { typeof(string) });
 
             if (parseMethod != null)
             {
                 var value = parseMethod.Invoke(null, new string[] { input });
-                return (value is TOut ? (TOut)value : defaultValue);
+                return (value is T ? (T)value : defaultValue);
             }
             else { return defaultValue; }
         }
 
-        public static bool TryParseOrDefault<TOut>(this string input, out TOut output)
+        /// <summary>
+        /// If the specified type has a TryParse(string, out Type) method, it will be invoked on the given input and the result returned.
+        /// </summary>
+        /// <typeparam name="T">The type to examine for a TryParse(string, out Type) method.</typeparam>
+        /// <param name="input">The System.String to parse.</param>
+        /// <param name="result">
+        /// When this method returns, contains the value equivalent of that represented in input,
+        /// if the conversion succeeded, or default(T) if the conversion failed. The conversion
+        /// fails if the input parameter is null or System.String.Empty, is not of the correct
+        /// format, or represents an invalid value for the specified type. This parameter is passed
+        /// uninitialized; any value originally supplied in result will be overwritten.
+        /// </param>
+        /// <returns>true if input was converted successfully; otherwise, false.</returns>
+        public static bool TryParseOrDefault<T>(this string input, out T result)
         {
-            return input.TryParseOrDefault(out output, default(TOut));
+            return input.TryParseOrDefault(out result, default(T));
         }
 
-        public static bool TryParseOrDefault<TOut>(this string input, out TOut output, TOut defaultValue)
+        /// <summary>
+        /// If the specified type has a TryParse(string, out Type) method, it will be invoked on the given input and the result returned.
+        /// </summary>
+        /// <typeparam name="T">The type to examine for a TryParse(string, out Type) method.</typeparam>
+        /// <param name="input">The System.String to parse.</param>
+        /// <param name="result">
+        /// When this method returns, contains the value equivalent of that represented in input,
+        /// if the conversion succeeded, or defaultValue if the conversion failed. The conversion
+        /// fails if the input parameter is null or System.String.Empty, is not of the correct
+        /// format, or represents an invalid value for the specified type. This parameter is passed
+        /// uninitialized; any value originally supplied in result will be overwritten.
+        /// </param>
+        /// <param name="defaultValue">A value to return in case the specified type has no TryParse(string, out Type) method.</param>
+        /// <returns>true if input was converted successfully; otherwise, false.</returns>
+        public static bool TryParseOrDefault<T>(this string input, out T result, T defaultValue)
         {
-            output = defaultValue;
+            result = defaultValue;
 
-            Type type = typeof(TOut);
-            MethodInfo parseMethod = type.GetTypeInfo().GetMethod(
+            var type = typeof(T);
+            var parseMethod = type.GetTypeInfo().GetMethod(
                 "TryParse",
-                new Type[] { typeof(string), typeof(TOut).MakeByRefType() });
+                new Type[] { typeof(string), typeof(T).MakeByRefType() });
 
             if (parseMethod != null)
             {
-                object[] parameters = new object[] { input, output };
+                var parameters = new object[] { input, result };
                 var value = parseMethod.Invoke(null, parameters);
 
                 if (value is bool)
@@ -58,7 +108,7 @@ namespace Extenso.Reflection
                     bool successful = (bool)value;
                     if (successful)
                     {
-                        output = (TOut)parameters[1];
+                        result = (T)parameters[1];
                         return true;
                     }
                 }
